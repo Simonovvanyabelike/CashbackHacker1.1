4218ee518c3d67c6b82dee1277c9abadb9e4a25a
simonovvanya <ivankartofan@lenta.ru>
2025-02-24 13:21:50 +0300

    Onemore Commit TestNG

--- src/test/java/ru/netology/service/CashbackHackServiceTestNG.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTestNG.java
@@ -1,4 +1,21 @@
 package ru.netology.service;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+
 public class CashbackHackServiceTestNG {
+    @Test
+    public void testRemainWhenAmountIs900() {
+        CashbackHackService service = new CashbackHackService();
+        int result = service.remain(900);
+        Assert.assertEquals(result, 100);
+    }
+
+    @Test
+    public void testRemainWhenAmountIs1000() {
+        CashbackHackService service = new CashbackHackService();
+        int result = service.remain(1000);
+        Assert.assertEquals(result, 0); // Это тест, который должен упасть
+    }
 }
